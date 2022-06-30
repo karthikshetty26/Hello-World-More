69b06d8f456e92cd9bc41bbd333a0d8dc377357e
Karthik Shetty <54941058+karthik-lezer@users.noreply.github.com>
2022-06-30 19:31:12 +0530

    C file added.
    C file added.

---
+++ C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main() {
+    printf("Hello World");
+}
+
+// If we are using int main () then:
+
+// #include <stdio.h>
+
+// int main() {
+//     print("Hello World");
+//     return 0;
+// }
